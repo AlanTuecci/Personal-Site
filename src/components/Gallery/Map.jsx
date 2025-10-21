2617fe320fdad14e83c2adbc04d94ec19d5a390a
Alan Tuecci <93105057+AlanTuecci@users.noreply.github.com>
2025-10-21 18:10:09 -0400

    Version 0.23.1
    Simplifies the Google Maps embed URL, updates iframe attributes for better security and compatibility, and removes unnecessary classes. Also bumps project version to 0.23.1 in package files.

--- src/components/Gallery/Map.jsx
+++ src/components/Gallery/Map.jsx
@@ -1,7 +1,8 @@
 const Map = ({ location_plus_code }) => {
   let mapSrc = null;
+
   if (location_plus_code) {
-    mapSrc = `https://maps.google.com/maps?width=600&height=400&hl=en&q=${encodeURIComponent(
+    mapSrc = `https://maps.google.com/maps?q=${encodeURIComponent(
       location_plus_code
     )}&t=&z=16&ie=UTF8&iwloc=B&output=embed`;
   }
@@ -18,13 +19,16 @@ const Map = ({ location_plus_code }) => {
 
   return (
     <div className="flex justify-center my-4 mx-4 lg:mx-10 mb-10">
-      <div className="bg-transparent h-[100%] w-full lg:w-[80%]">
+      <div className="w-full lg:w-[80%]">
         <iframe
           key={mapSrc}
-          className="w-[100%] h-[100%] min-h-72 rounded-lg shadow-xl"
+          className="w-full min-h-72 rounded-lg shadow-xl"
           frameBorder="0"
           scrolling="no"
           src={mapSrc}
+          referrerPolicy="no-referrer-when-downgrade"
+          allowFullScreen
+          sandbox="allow-scripts allow-same-origin allow-popups"
         ></iframe>
       </div>
     </div>
